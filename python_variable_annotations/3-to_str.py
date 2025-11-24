858e5c03dc1e6a99042ff2b523441a46b77af00a
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-11-24 15:22:52 +0100

    add: function 3-to_str added

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+Function that return the string representation of a float
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Return the string representation of n
+    """
+    return str(n)

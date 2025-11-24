caa467906cfdfacba867e5b2957577c05806b6a3
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-11-24 15:16:27 +0100

    add: function 2-floor added

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+Function that return the floor of a float
+"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Return the floor of n
+    """
+    return math.floor(n)

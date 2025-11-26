87a89c0e80aa32aa95846844d38818b8c95ddba7
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-11-26 10:31:26 +0100

    add: documentation

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
 """
-Function that return a function that multiplies a float
+Function that return a documented function
 """
 
 from typing import Iterable, List, Tuple, Sequence
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+    Element length functon documented
+    """
     return [(i, len(i)) for i in lst]

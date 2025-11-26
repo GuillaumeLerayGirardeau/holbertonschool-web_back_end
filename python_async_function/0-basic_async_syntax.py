c7e3bb71f90617f1f4fb09401e21c4c877d1cf20
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-11-26 11:19:18 +0100

    add: function basic_async_syntax added

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""
+Function that wait a random amount of time before printing it
+"""
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay=10):
+    """
+    Wait a random amount of time between 0 and max_delay
+    """
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay

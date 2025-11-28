20649f2fa6af6bb9ff6e4c051102fa70f142e7ff
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-11-28 11:06:52 +0100

    fix: deleted print test line

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -16,7 +16,6 @@ def measure_time(n: int, max_delay: int) -> float:
     Measures the total execution time for wait_n
     """
     start_time = time.time()
-    print(start_time)
     asyncio.run(wait_n(n, max_delay))
     total_time = time.time() - start_time
     return total_time / n

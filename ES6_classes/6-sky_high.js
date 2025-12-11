17c6ecebdf62f0b9db279a6da138f4620d7a105d
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-12-11 18:17:49 +0100

    add: class sky_high added

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,24 @@
+import Building from "./5-building";
+
+export default class SkyHighBuilding extends Building {
+    constructor(sqft, floors) {
+        super(sqft);
+        this._floors = floors;
+    }
+
+    set floors (floors) {
+        if (typeof floors === 'number') {
+            this._floors = floors;
+        } else {
+            throw new TypeError("floors must be a number");
+        }
+    }
+
+    get floors() {
+        return this._floors;
+    }
+
+    evacuationWarningMessage () {
+        return `Evacuate slowly the ${this._floors} floors`;
+    }
+}

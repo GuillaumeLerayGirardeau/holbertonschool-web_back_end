18f6c22e69d9fcb4752af2f4f9111db5fb313da0
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-12-11 19:22:15 +0100

    fix: removed getters and setters

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,47 +1,11 @@
 export default class Car {
     constructor(brand, motor, color) {
-        this.brand = brand;
-        this.motor = motor;
-        this.color = color;
-    }
-
-    set brand(brand) {
-        if(typeof brand === 'string') {
-            this._brand = brand;
-        } else {
-            throw new TypeError("brand must be a string");
-        }
-    }
-
-    get brand () {
-        return this._brand;
-    }
-
-    set motor(motor) {
-        if (typeof motor === 'string') {
-            this._motor = motor;
-        } else {
-            throw new TypeError("motor must be a string");
-        }
-    }
-
-    get motor() {
-        return this._motor;
-    }
-
-    set color(color) {
-        if (typeof color === 'string') {
-            this._color = color;
-        } else {
-            throw new TypeError("color must be a string");
-        }
-    }
-
-    get color() {
-        return this._color;
+        this._brand = brand;
+        this._motor = motor;
+        this._color = color;
     }
 
     cloneCar () {
-        return new this.constructor(this.brand, this.motor, this.color);
+        return new this.constructor(this._brand, this._motor, this._color);
     }
 }

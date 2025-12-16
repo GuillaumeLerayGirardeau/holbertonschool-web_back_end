3d834e7c2f61b50a71a1a6a37a05334543209eb6
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-12-16 16:04:55 +0100

    add: function 3-all added

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,12 @@
+import { uploadPhoto } from "./utils.js";
+import { createUser } from "./utils.js";
+
+export default async function handleProfileSignup() {
+    
+    try {
+        const values = await Promise.all([uploadPhoto(), createUser()]);
+        console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
+    } catch {
+        console.log('Signup system offline');
+    };
+}

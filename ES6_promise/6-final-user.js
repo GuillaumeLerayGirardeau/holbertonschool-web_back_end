105a5e399cbdeb2fd23e8d7e55a407258e9e64ad
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-12-17 18:14:21 +0100

    fix: corrected syntax

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -4,7 +4,15 @@ import uploadPhoto from "./5-photo-reject.js";
 export default async function handleProfileSignup(firstName, lastName, fileName) {
     try {
         const value = await Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
-        return [{'status': value[0].status, 'value': value[0].value}, {'status': value[1].status, 'value': value[1].value}];
+        return value.map(object => {
+            let value;
+            if(object.status == 'fulfilled') {
+                value = object.value;
+            } else {
+                value = String(object.reason);
+            }
+            return {'status': object.status, 'value': value};
+        })
     } catch(error) {
         throw new Error(error);
     }

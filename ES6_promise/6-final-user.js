106c50649bfd0187f640450b558705f41e49fafb
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-12-17 18:01:56 +0100

    fix: wrong array expected

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -4,7 +4,7 @@ import uploadPhoto from "./5-photo-reject.js";
 export default async function handleProfileSignup(firstName, lastName, fileName) {
     try {
         const value = await Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
-        return [{'status': signUpUser.status, 'value': signUpUser.value}, {'status': uploadPhoto.status, 'value': uploadPhoto.value}];
+        return [{'status': value[0].status, 'value': value[0].value}, {'status': value[1].status, 'value': value[1].value}];
     } catch(error) {
         throw new Error(error);
     }

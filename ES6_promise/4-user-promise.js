5b337b225062a5cac63ad384d6474363c8cf4d5d
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-12-17 17:35:51 +0100

    fix: use the Promise syntax

--- ES6_promise/4-user-promise.js
+++ ES6_promise/4-user-promise.js
@@ -1,5 +1,5 @@
-export default async function signUpUser(firstName, lastName) {
+export default function signUpUser(firstName, lastName) {
 
-    return {'firstName': firstName, 'lastName': lastName};
+    return Promise.resolve({'firstName': firstName, 'lastName': lastName});
 
 }

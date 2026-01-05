4021a530e3d64e6d8b1b67b8c2ad9d9aa44b9a4a
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2026-01-05 15:38:34 +0100

    fix: corrected indentation

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,2 +1,6 @@
-process.stdin.on('string');
-console.log(string);
+console.log("Welcome to Holberton School, what is your name?");
+process.stdin.on('data', data => {
+    data = data.toString();
+    process.stdout.write(`Your name is: ${data}`)
+});
+console.log('This important software is now closing');

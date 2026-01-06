cd1d946a4dda2ddf59864856723489b8fdaec413
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2026-01-06 14:21:42 +0100

    add: function 1 added

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,6 +1,8 @@
-console.log("Welcome to Holberton School, what is your name?");
-process.stdin.on('data', data => {
-    data = data.toString();
-    process.stdout.write(`Your name is: ${data}`)
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('data', (data) => {
+  const input = data.toString();
+  process.stdout.write(`Your name is: ${input}`);
 });
-console.log('This important software is now closing');
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});

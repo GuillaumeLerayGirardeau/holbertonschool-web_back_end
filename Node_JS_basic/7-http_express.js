d15e654a835181712bc462ec86622f78e13122ab
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2026-01-07 15:55:05 +0100

    add: function 7 added

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+const port = 1245;
+
+const databasePath = process.argv[2];
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+  let output = 'This is the list of our students\n';
+
+  try {
+    const logs = [];
+    const originalLog = console.log;
+
+    console.log = (...args) => {
+      logs.push(args.join(' '));
+      originalLog(...args);
+    };
+
+    await countStudents(databasePath);
+
+    console.log = originalLog;
+
+    output += logs.join('\n');
+    res.send(output);
+  } catch (err) {
+    console.log = originalLog;
+    res.send('Cannot load the database');
+  }
+});
+
+
+app.listen(port, () => {
+  console.log(`Server running at port ${port}`);
+});
+
+module.exports = app;

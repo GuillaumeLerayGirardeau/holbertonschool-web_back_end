906a730e91b1c293dba511fea3a678c53fdd727c
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2026-01-07 15:17:30 +0100

    fix: making the output of an old code as the visible output

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,5 +1,5 @@
 const http = require('node:http');
-const fs = require('node:fs').promises;
+const countStudents = require('./3-read_file_async');
 
 const hostname = '127.0.0.1';
 const port = 1245;
@@ -16,32 +16,15 @@ const app = http.createServer(async (req, res) => {
   if (req.url === '/students') {
     res.writeHead(200, { 'Content-type': 'text/plain' });
     try {
-      const studentData = await fs.readFile(databasePath, 'utf8');
-      res.write('This is the list of our students\n');
-      const allStudent = studentData.split('\n').slice(1).filter(
-        (student) => student.length > 0,
-      );
-      res.write(`Number of students: ${allStudent.length}\n`);
-
-      const allSeparatedDatas = [];
-      for (const student of allStudent) {
-        const column = student.split(',');
-        allSeparatedDatas.push(column);
-      }
-      const fields = {};
-      for (const element of allSeparatedDatas) {
-        const firstname = element[0];
-        const field = element[3];
-
-        if (!fields[field]) fields[field] = [];
-
-        fields[field].push(firstname);
-      }
-      const lines = Object.keys(fields).map(
-        (field) => `Number of students in ${field}: ${
-          fields[field].length}. List: ${fields[field].join(', ')}`,
-      );
-      res.write(lines.join('\n'));
+      const logs = [];
+      const originalLog = console.log;
+      console.log = (...args) => {
+        logs.push(args.join(' '));
+        originalLog(...args);
+      };
+      await countStudents(databasePath);
+      console.log = originalLog;
+      res.write(logs.join('\n'));
     } catch (error) {
       return res.end('Cannot load the database');
     }
